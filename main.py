238e750db8e8a37f9f2e781effe2bf63e9308fa1
Harshit Patil <85615003+harshitpatilx@users.noreply.github.com>
2021-09-27 11:56:38 +0530

    Added Comments..
    added comments and Docstring in the roll_dice() function

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 # Imporing Modules
 from random import randint
 
+# Roll Dice Function
 def roll_dice():
+    '''
+    Rolls the Dice, and returns a random number between 1 and 6
+    '''
     print("Rolling the Dice...")
     dice_result = randint(1, 6)
     return f"{dice_result}"
 
+# Mail Loop
 while True:
     a = input("Press Enter to Roll the Dice: ")
     if a == "":
